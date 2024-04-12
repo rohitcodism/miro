4b37ab5c5eade8064cd31df0b91b981d177519c2
rohitcodism <rohitpaula18i@gmail.com>
2024-04-13 00:18:56 +0530

    ✅ Update OrgSideBar component in dashboard

--- app/(dashboard)/_components/OrgSideBar.tsx
+++ app/(dashboard)/_components/OrgSideBar.tsx
@@ -1,8 +1,26 @@
 'use client'
 
-import React from 'react'
+import React from 'react';
+import Link from 'next/link';
+import Image from 'next/image';
+import { Poppins } from 'next/font/google';
+import { miroIcon } from '@/assets';
+import { cn } from '@/lib/utils';
+import { OrganizationSwitcher } from '@clerk/nextjs';
+import { Button } from '@/components/ui/button';
+import { LayoutDashboard, Star } from 'lucide-react';
+import { useSearchParams } from 'next/navigation';
+
+const font = Poppins({
+    subsets: ["latin"],
+    weight: ["600"]
+})
 
 function OrgSideBar() {
+
+    const searchParams = useSearchParams();
+    const favorites = searchParams.get("favorites");
+
     return (
         <div
             className='
@@ -13,10 +31,110 @@ function OrgSideBar() {
                 w-[260px]
                 pl-5
                 pt-5
-                bg-red-500
             '
         >
-            OrgSideBar
+            <Link
+                href={"/"}
+            >
+                <div
+                    className='
+                        flex
+                        items-center
+                        gap-x-2
+                    '
+                >
+                    <Image 
+                        src={miroIcon}
+                        alt='logo-miro'
+                        height={25}
+                        width={25}
+                    />
+                    <span
+                        className={cn(
+                            "font-semibold text-2xl",
+                            font.className
+                        )}
+                    >
+                        Miro
+                    </span>
+                </div>
+            </Link>
+            <OrganizationSwitcher 
+                hidePersonal
+                appearance={{
+                    elements: {
+                        rootBox: {
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            width: "100%",
+                        },
+                        organizationSwitcherTrigger: {
+                            padding: "6px",
+                            width: "100%",
+                            borderRadius: "8px",
+                            border: "1px solid #E5E7EB",
+                            justifyContent : "space-between",
+                            backgroundColor: "white",
+                        }
+                    }
+                }}
+            />
+            <div className="space-y-1 w-full">
+                <Button
+                    variant={favorites ? 'ghost' : 'secondary'}
+                    asChild
+                    size={'lg'}
+                    className='
+                        font-normal
+                        justify-start
+                        px-2
+                        w-full
+                    '
+                >
+                    <Link
+                        href={"/"}
+                    >
+                        <LayoutDashboard 
+                            className='
+                                h-4
+                                w-4
+                                mr-2
+                            '
+                        />
+                        Team boards
+                    </Link>
+                </Button>
+                <Button
+                    variant={favorites ? 'secondary' : 'ghost'}
+                    asChild
+                    size={'lg'}
+                    className='
+                        font-normal
+                        justify-start
+                        px-2
+                        w-full
+                    '
+                >
+                    <Link
+                        href={{
+                            pathname: "/",
+                            query: {
+                                favorites: true,
+                            }
+                        }}
+                    >
+                        <Star 
+                            className='
+                                h-4
+                                w-4
+                                mr-2
+                            '
+                        />
+                        Favorite boards
+                    </Link>
+                </Button>
+            </div>
         </div>
     );
 }
