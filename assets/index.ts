d304e4a6c00c184468a83502fa87a73978aca3d7
rohitcodism <rohitpaula18i@gmail.com>
2024-04-19 00:14:45 +0530

    ✅ Add new SVG assets and update imports in index.ts

--- assets/index.ts
+++ assets/index.ts
@@ -1,4 +1,7 @@
 import miroIcon from "./miro-svgrepo-com.svg";
 import elements from "./elements.svg"
+import emptySearch from "./empty-search.svg";
+import emptyFavorites from "./empty-favorites.svg";
+import emptyBoards from "./note.svg";
 
-export { miroIcon, elements }
+export { miroIcon, elements, emptyBoards, emptyFavorites, emptySearch }
