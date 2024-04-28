ae29f7017549ac88fa7076cae51c40729249c284
rohitcodism <rohitpaula18i@gmail.com>
2024-04-28 21:03:20 +0530

    🚀 Add BoardCard component to BoardList for displaying individual board cards

--- app/(dashboard)/_components/BoardList.tsx
+++ app/(dashboard)/_components/BoardList.tsx
@@ -5,6 +5,10 @@ import React from 'react'
 import EmptySearch from './emptySearch';
 import EmptyFavorites from './emptyFavorites';
 import EmptyBoard from './emptyBoard';
+import { useQuery } from 'convex/react';
+import { api } from '@/convex/_generated/api';
+import { Loader2 } from 'lucide-react';
+import { BoardCard } from './BoardCard';
 
 interface BoardListProps {
     orgId: string,
@@ -16,7 +20,23 @@ interface BoardListProps {
 
 function BoardList({orgId, query}: BoardListProps) {
 
-    const data = []; //TODO: Change to API call
+    const data = useQuery(api.boards.get, {orgId})
+
+    if(data === undefined){
+        return(
+            <div
+                className='min-w-[100%] min-h-[100%] flex justify-center items-center'
+            >
+                <Loader2  
+                    className='
+                        w-16
+                        h-16
+                        animate-spin
+                    '
+                />
+            </div>
+        );
+    }
 
     if(!data?.length && query.search){
         return <EmptySearch />
@@ -32,7 +52,43 @@ function BoardList({orgId, query}: BoardListProps) {
 
     return (
         <div>
-            
+            <h2
+                className='
+                    text-3xl
+                    font-bold
+                '
+            >
+                {query.favorites ? "Favorite boards" : "Team boards"}
+            </h2>
+            <div
+                className='
+                    grid
+                    grid-cols-1
+                    sm:grid-cols-1
+                    md:grid-cols-4
+                    lg:grid-cols-4
+                    xl:grid-cols-5
+                    2xl:grid-cols-6
+                    gap-5
+                    mt-8
+                    pb-10
+                '
+            >
+                {
+                    data.map((board) => (
+                        <BoardCard 
+                            key={board._id}
+                            id={board._id}
+                            title={board.title}
+                            imageUrl={board.imageUrl}
+                            authorId={board.authorId}
+                            authorName={board.authorName}
+                            at={board._creationTime}
+                            isFavorite={false}
+                        />
+                    ))
+                }
+            </div>
         </div>
     )
 }
