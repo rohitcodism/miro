c54c8216c0c17806ad759bdc80d0dca9d58b9b20
rohitcodism <rohitpaula18i@gmail.com>
2024-04-28 21:03:13 +0530

    🚀 Update SVG imports in index.ts

--- assets/index.ts
+++ assets/index.ts
@@ -3,20 +3,5 @@ import elements from "./elements.svg"
 import emptySearch from "./empty-search.svg";
 import emptyFavorites from "./empty-favorites.svg";
 import emptyBoards from "./note.svg";
-import svg1 from "./placeholders/1.svg";
-import svg2 from "./placeholders/2.svg";
-import svg3 from "./placeholders/3.svg";
-import svg4 from "./placeholders/4.svg";
-import svg5 from "./placeholders/5.svg";
-import svg6 from "./placeholders/6.svg";
-import svg7 from "./placeholders/7.svg";
-import svg8 from "./placeholders/8.svg";
-import svg9 from "./placeholders/9.svg";
-import svg10 from "./placeholders/10.svg";
-import svg11 from "./placeholders/11.svg";
-import svg12 from "./placeholders/12.svg";
-import svg13 from "./placeholders/13.svg";
-import svg14 from "./placeholders/14.svg";
-import svg15 from "./placeholders/15.svg";
 
-export { miroIcon, elements, emptyBoards, emptyFavorites, emptySearch, svg1, svg2, svg3, svg4, svg5, svg6, svg7, svg8, svg9, svg10, svg11, svg12, svg13, svg14, svg15}
+export { miroIcon, elements, emptyBoards, emptyFavorites, emptySearch}
