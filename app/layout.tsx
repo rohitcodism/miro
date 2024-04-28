3dc455a1d734bd531c25393eab5186230a01245b
rohitcodism <rohitpaula18i@gmail.com>
2024-04-28 15:36:23 +0530

    🚀 Add Toaster component to RootLayout in layout.tsx ✅ Update package-lock.json with @radix-ui/react-toast dependency ✅ Update package.json with @radix-ui/react-toast dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ConvexCLientProvider } from "@/provider/convex-client-provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,6 +22,7 @@ export default function RootLayout({
           <ConvexCLientProvider>
             {children}
           </ConvexCLientProvider>
+          <Toaster />
         </body>
       </html>
   );
