26ec0b2ca254f4c2d3447f7743c54d1209a10882
rohitcodism <rohitpaula18i@gmail.com>
2024-04-30 12:40:50 +0530

    🚀 Update useQuery call in BoardList.tsx to include additional query parameters

--- app/(dashboard)/_components/BoardList.tsx
+++ app/(dashboard)/_components/BoardList.tsx
@@ -21,7 +21,7 @@ interface BoardListProps {
 
 function BoardList({ orgId, query }: BoardListProps) {
 
-    const data = useQuery(api.boards.get, { orgId })
+    const data = useQuery(api.boards.get, { orgId, ...query })
 
     if (data === undefined) {
         return (
