9f135752757e1719a698500841fcdd2e4493af55
rohitcodism <rohitpaula18i@gmail.com>
2024-05-01 15:17:49 +0530

    ✅ 🔧 Update Info component to allow board title renaming

--- app/board/[boardId]/_components/Info.tsx
+++ app/board/[boardId]/_components/Info.tsx
@@ -1,10 +1,88 @@
 'use client'
 
-interface InfoProps {
+import { api } from "@/convex/_generated/api";
+import { Id } from "@/convex/_generated/dataModel";
+import { useMutation, useQuery } from "convex/react";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { miroIcon } from "@/assets";
+import Link from "next/link";
+import Hint from "@/components/Hint";
+import {
+    Dialog,
+    DialogClose,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle,
+    DialogTrigger,
+} from "@/components/ui/dialog";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { FormEvent, useState } from "react";
+import { toast } from "@/components/ui/use-toast";
 
+interface InfoProps {
+    boardId: string,
 }
 
-export const Info = () => {
+const font = Poppins({
+    subsets: ['latin'],
+    weight: ['600']
+})
+
+export const TextSeparator = () => {
+    return (
+        <div className="text-neutral-300 px-1.5">
+            |
+        </div>
+    );
+}
+
+export const Info = ({
+    boardId
+}: InfoProps) => {
+
+    const editTitle = useMutation(api.board.editTitle)
+
+    const boardData = useQuery(api.board.get, { boardId: boardId as Id<"boards"> }) //TODO: Learn about useQuery
+
+    const [formData, setFormData] = useState({ title: "" });
+
+    if (!boardData) return <InfoSkeleton />;
+
+
+    const handleSave = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const newTitle = formData.title;
+
+        try {
+            editTitle({ id: boardData._id as Id<'boards'>, title: formData.title })
+
+                .then(() => toast({
+                    title: "Renamed!",
+                    description: `${boardData.title} board renamed to ${newTitle} board`
+                }))
+                .catch(() => toast({
+                    title: "Error!",
+                    description: `Something went wrong while renaming the ${boardData.title} board`,
+                    variant: 'destructive'
+                }))
+
+        } catch (error) {
+            toast({
+                title: "Error!",
+                description: `Something went wrong while renaming the ${boardData.title} board`,
+                variant: 'destructive'
+            })
+        }
+
+        formData.title = "";
+    };
+
     return (
         <div
             className="
@@ -20,7 +98,77 @@ export const Info = () => {
                 shadow-md
             "
         >
-            Info //TODO: Information about the board
+            <Hint
+                label="Back to Home"
+                side="bottom"
+                sideOffset={15}
+            >
+                <Link
+                    href={'/'}
+                >
+                    <Button
+                        variant={'board'}
+                        className="
+                    px-2
+                "
+                    >
+                        <Image
+                            src={miroIcon}
+                            alt="miro-icon"
+                            height={30}
+                            width={30}
+                        />
+                        <span
+                            className={
+                                cn(
+                                    "font-semibold text-xl ml-2 text-black",
+                                    font.className
+                                )
+                            }
+                        >
+                            Miro
+                        </span>
+                    </Button>
+                </Link>
+            </Hint>
+            <TextSeparator />
+            <Dialog>
+                <DialogTrigger>
+                    <Button
+                        variant={'board'}
+                        className="
+                    text-base
+                    font-semibold
+                    px-2
+                "
+                    >
+                        {boardData.title}
+                    </Button>
+                </DialogTrigger>
+                <DialogContent>
+                    <DialogHeader>
+                        <DialogTitle>
+                            Rename board
+                        </DialogTitle>
+                    </DialogHeader>
+                    <form onSubmit={handleSave}>
+                                <div className="flex flex-col py-2 gap-y-4">
+                                    <Label>Title</Label>
+                                    <Input
+                                        id="title"
+                                        placeholder="New board title"
+                                        value={formData.title}
+                                        onChange={(e) => setFormData({ ...formData, title: e.target.value })}
+                                    />
+                                    <DialogClose
+                                        onClick={(e) => e.stopPropagation()}
+                                    >
+                                        <Button type="submit">Save Changes</Button>
+                                    </DialogClose>
+                                </div>
+                            </form>
+                </DialogContent>
+            </Dialog>
         </div>
     );
 }
