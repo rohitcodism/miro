5557df83442fbc3c0d2637a4a10dd05a69aea7f2
rohitcodism <rohitpaula18i@gmail.com>
2024-05-06 00:45:54 +0530

    feat: Add types for canvas state and layer types
    This commit adds TypeScript types for the canvas state and layer types in the `Canvas.ts` file. These types define the structure and properties of different layers and the canvas state. They will be used for type checking and ensuring consistency in the canvas component.
    
    Note: No newline at end of file

---
+++ Types/Canvas.ts
@@ -0,0 +1,130 @@
+//TODO: Learn more about the working of this canvas state
+export type Color = {
+    r: number,
+    g: number,
+    b: number
+}
+
+export type Camera = {
+    x: number,
+    y: number
+}
+
+export type Point = {
+    x: number,
+    y: number
+}
+
+export type XYWH = {
+    x: number,
+    y: number,
+    width: number,
+    height: number
+}
+
+export enum Side {
+    Top = 1,
+    Bottom = 2,
+    Left = 4,
+    Right = 8
+}
+
+export enum LayerType  {
+    Rectangle,
+    Ellipse,
+    Path,
+    Text,
+    Note
+}
+
+export type RectangleLayer = {
+    type: LayerType.Rectangle,
+    x: number,
+    y: number,
+    height: number,
+    width: number,
+    fill: Color,
+    value?: string,
+}
+
+export type EllipseLayer = {
+    type: LayerType.Ellipse,
+    x: number,
+    y: number,
+    height: number,
+    width: number,
+    fill: Color,
+    value?: string,
+}
+
+export type PathLayer = {
+    type: LayerType.Path,
+    x: number,
+    y: number,
+    height: number,
+    width: number,
+    fill: Color,
+    points: number[][],
+    value?: string,
+}
+
+export type TextLayer = {
+    type: LayerType.Text,
+    x: number,
+    y: number,
+    height: number,
+    width: number,
+    fill: Color,
+    value?: string,
+}
+
+export type NoteLayer = {
+    type: LayerType.Note,
+    x: number,
+    y: number,
+    height: number,
+    width: number,
+    fill: Color,
+    value?: string,
+}
+
+export type CanvasState =
+    | 
+        {
+            mode: CanvasMode.none
+        }
+    |   {
+            mode: CanvasMode.Pressing
+            origin: Point,
+        }
+    |   {
+            mode: CanvasMode.SelectionNet,
+            origin: Point,
+            current?: Point
+        }
+    |   {
+            mode: CanvasMode.Translating,
+            current: Point
+        }
+    |   {
+            mode: CanvasMode.Resizing
+            initialBounds: XYWH,
+            corner: Side
+        }
+    |   {
+            mode: CanvasMode.Inserting
+            layerType: LayerType.Ellipse | LayerType.Rectangle | LayerType.Text | LayerType.Note
+        }
+    |   {
+            mode: CanvasMode.Pencil
+        }
+
+export enum CanvasMode {
+    none,
+    Pressing,
+    Inserting,
+    SelectionNet,
+    Translating,
+    Resizing,
+    Pencil
+}
