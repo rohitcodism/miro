1015025e93f9bcfd44a2c59f51c8d5dcfabd52b5
rohitcodism <rohitpaula18i@gmail.com>
2024-05-15 14:56:03 +0530

    refactor: Add ColorToCSS utility function for converting Color object to CSS color code

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,4 @@
-import { Camera } from "@/Types/Canvas";
+import { Camera, Color } from "@/Types/Canvas";
 import { type ClassValue, clsx } from "clsx"
 import { twMerge } from "tailwind-merge";
 
@@ -26,4 +26,8 @@ export function pointerEventToCanvasPoint(
     x: Math.round(e.clientX) - camera.x,
     y: Math.round(e.clientY) - camera.y
   }
+}
+
+export function ColorToCSS(color: Color){
+  return `#${color.r.toString(16).padStart(2, "0")}${color.g.toString(16).padStart(2, "0")}${color.b.toString(16).padStart(2, "0")}`
 }
