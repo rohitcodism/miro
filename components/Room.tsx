d06e765933b3e9b1fa211cc13a150f7b7bd9d54e
rohitcodism <rohitpaula18i@gmail.com>
2024-05-15 13:39:08 +0530

    feat: Add LiveList and LiveMap to Room initialStorage
    This commit adds the LiveList and LiveMap classes from the @liveblocks/client library to the initialStorage prop in the Room component. The LiveList is used to store layerIds, while the LiveMap is used to store layers. This change enhances the functionality of the Room component by providing real-time synchronization and collaboration capabilities for layers in the canvas.

--- components/Room.tsx
+++ components/Room.tsx
@@ -4,6 +4,8 @@ import { ReactNode } from "react";
 import { ClientSideSuspense } from "@liveblocks/react";
 
 import { RoomProvider } from "@/liveblocks.config";
+import { LiveList, LiveMap, LiveObject } from "@liveblocks/client";
+import { Layer } from "@/Types/Canvas";
 
 interface RoomProps {
     children: ReactNode,
@@ -20,7 +22,12 @@ export const Room = ({
         <RoomProvider
             id={roomId}
             initialPresence={{
-                cursor: null
+                cursor: null,
+                selection: []
+            }}
+            initialStorage={{
+                layers: new LiveMap<string, LiveObject<Layer>>,
+                layerIds: new LiveList()
             }}
         >
             <ClientSideSuspense
