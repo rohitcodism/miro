0992e3b139af6d5aae5d2333a1aa776b838fd1f7
rohitcodism <rohitpaula18i@gmail.com>
2024-05-17 10:16:27 +0530

    feat: Add continueDrawing and insertPath functions for pencil drawing
    This commit adds the continueDrawing and insertPath functions to the Canvas component. The continueDrawing function is responsible for updating the pencil draft while the user is drawing on the canvas. It checks the current canvas mode, mouse button state, and pencil draft to determine if the drawing should continue. The insertPath function is used to insert the completed pencil drawing as a new layer on the canvas. It checks the validity of the pencil draft and the maximum number of layers before inserting the path layer. These new functions enhance the functionality of the canvas by allowing users to draw with a pencil tool and save their drawings as separate layers.
    
    Note: No newline at end of file

--- app/board/[boardId]/_components/CursorPresence.tsx
+++ app/board/[boardId]/_components/CursorPresence.tsx
@@ -1,7 +1,10 @@
 'use client'
 import { memo } from "react";
-import { useOther, useOthersConnectionIds } from "@/liveblocks.config";
+import { useOther, useOthersConnectionIds, useOthersMapped } from "@/liveblocks.config";
 import { Cursor } from "./Cursor";
+import { shallow } from "@liveblocks/client";
+import { Path } from "./Path";
+import { ColorToCSS } from "@/lib/utils";
 
 const Cursors = () => {
     const ids = useOthersConnectionIds();
@@ -20,11 +23,39 @@ const Cursors = () => {
     );
 }
 
+const Drafts = () => {
+    const others = useOthersMapped((other) => ({
+        pencilDraft: other.presence.pencilDraft,
+        penColor: other.presence.penColor,
+    }), shallow)
+
+    return (
+        <>
+            {
+                others.map(([key, other]) => {
+                    if(other.pencilDraft){
+                        return(
+                            <Path 
+                                key={key}
+                                x={0}
+                                y={0}
+                                points={other.pencilDraft}
+                                fill={other.penColor ? ColorToCSS(other.penColor) : "#000"}
+                            />
+                        );
+                    }
+                    return null;
+                })
+            }
+        </>
+    );
+}
+
 
 export const CursorPresence = memo(() => {
     return(
         <>
-            { /* //TODO: Draft pencil component */}
+            <Drafts />
             <Cursors />
         </>
     );
