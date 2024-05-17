99f70109e7b1d3cea3f7d232aac5bd8e10dfeba1
rohitcodism <rohitpaula18i@gmail.com>
2024-05-17 10:15:49 +0530

    refactor: Update SelectionTools component to use immutable version of liveLayerIds

--- app/board/[boardId]/_components/SelectionTools.tsx
+++ app/board/[boardId]/_components/SelectionTools.tsx
@@ -29,7 +29,7 @@ export const SelectionTools = memo(({
 
         const indices: number[] = [];
 
-        const arr = liveLayerIds.toArray();
+        const arr = liveLayerIds.toImmutable();
 
         for(let i = 0; i < arr.length; i++){
             if(selection.includes(arr[i])) {
@@ -50,7 +50,7 @@ export const SelectionTools = memo(({
 
         const indices: number[] = [];
 
-        const arr = liveLayerIds.toArray();
+        const arr = liveLayerIds.toImmutable();
 
         for(let i = 0; i < arr.length; i++){
             if(selection.includes(arr[i])) {
